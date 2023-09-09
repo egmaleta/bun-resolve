def266668c2596c86bfcdd031be634fa89c4fbce
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-09-09 15:15:46 -0400

    first impl (dumb)
    replaces all instances of original string
    it should only replace the instances of the original string in import/require statements

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
-export function hello() {
-  console.log("Hello, Bun!");
+import { type BunPlugin, file } from "bun";
+
+type PluginOptions = {
+  alias: Record<string, string>;
+};
+
+function plugin(options?: PluginOptions): BunPlugin {
+  const alias = options?.alias;
+
+  return {
+    name: "bun-resolve",
+    setup(build) {
+      alias &&
+        build.onLoad({ filter: /\.(js|jsx|ts|tsx)$/ }, async (args) => {
+          // open file & read content
+          // for each alias entry: search entry.key and replace by entry.value
+          let contents = await file(args.path).text();
+          Object.entries(alias).forEach(([originalText, newText]) => {
+            contents = contents.replaceAll(originalText, newText);
+          });
+
+          return {
+            contents,
+            loader: args.loader,
+          };
+        });
+    },
+  };
 }
+
+export default plugin;
